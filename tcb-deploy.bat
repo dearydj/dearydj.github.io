f0b97a8647c0f126008acc728b0765148187046a
YDJ1991 <33014966+YDJ1991@users.noreply.github.com>
2022-07-21 23:54:51 +0800

    重新构建
    docs、public

--- tcb-deploy.bat
+++ tcb-deploy.bat
@@ -1,6 +1,6 @@
 cd %~dp0
-hugo -D
+hugo --config ./config.yml
 @REM tcb login
 @REM tcb login -k 1302593112 --apiKeyId AKIDmMhla9TZP0K3dxzkAKJ6ew0qTw96mY3G --apiKey 0kERO4UdYFTUGpAG6fy0nJZaNuCG6fy1
-cloudbase hosting deploy ./public -e env-cffgqqpx
+tcb hosting deploy ./public -e env-cffgqqpx
 pause
