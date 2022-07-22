24d0c9c3e2f9a1ca3cbeb52c5aa0bca96b2cf1a7
YDJ1991 <33014966+YDJ1991@users.noreply.github.com>
2022-07-22 12:02:33 +0800

    尝试重命名

--- tcb-deploy.bat
+++ tcb-deploy.bat
@@ -1,6 +1,5 @@
 cd %~dp0
 hugo --config ./config.yml
 @REM tcb login
-@REM tcb login -k 1302593112 --apiKeyId AKIDmMhla9TZP0K3dxzkAKJ6ew0qTw96mY3G --apiKey 0kERO4UdYFTUGpAG6fy0nJZaNuCG6fy1
 tcb hosting deploy ./public -e env-cffgqqpx
 pause
